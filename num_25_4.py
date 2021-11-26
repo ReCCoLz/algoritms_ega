079327c0cfd75e219e75606e135de1a1d0a87048
ReCCoLz <48411138+ReCCoLz@users.noreply.github.com>
2021-11-26 15:17:18 +0300

    Create num_25_4.py

---
+++ num_25_4.py
@@ -0,0 +1,6 @@
+numb = int(input("Введите целое число: "))
+count_of_dividers = 0
+for i in range(numb - 1, 1, -1):
+    if (numb % i == 0):
+        count_of_dividers += 1
+print("Количество равно:", count_of_dividers)
