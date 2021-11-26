4b543e8e63d7e008fe4fe2162d9bd7a3d56491c2
ReCCoLz <48411138+ReCCoLz@users.noreply.github.com>
2021-11-26 15:21:41 +0300

    Update and rename num_25.py to vse_deliteli_celogo_chisla.py

--- vse_deliteli_celogo_chisla.py
+++ vse_deliteli_celogo_chisla.py
@@ -1,5 +1,5 @@
 numb = int(input("Введите целое число: "))
 print("Результат:", end = " ")
-for i in range(numb - 1, 1, -1):
+for i in range(numb - 1, 1, -1): #Перебираем все числа, уменьшая начальное на 1
     if (numb % i == 0):
         print(i, end = " ")
